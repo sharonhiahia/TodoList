732b68f3e23edcb911f5a0182cea31797df3a17c
RongX <sharoneaecs@gmail.com>
2019-07-04 13:31:01 -0700

    Initial Commit

---
+++ TodoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by Rong Xiao on 7/4/19.
+//  Copyright © 2019 Rong Xiao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
